1a3ca28c57ff82b47bcb5885a7769546c27b0db4
awesomesara <79391708+awesomesara@users.noreply.github.com>
2022-12-20 23:49:41 +0600

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "../pages/HomePage/HomePage";
+import Cart from "../pages/CartPage/CartPage";
+import CategoryProduct from "../pages/CategoryProductPage/CategoryProductPage";
+import ProductSingle from "./ProductSinglePage/ProductSinglePage";
+import Search from "./SearchPage/SearchPage";
+import Contact from "./ContactPage/ContactPage"
+
+export {Home, CategoryProduct, ProductSingle, Cart, Search, Contact};
